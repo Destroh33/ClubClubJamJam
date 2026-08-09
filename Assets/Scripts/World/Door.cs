bcc011c7f860326ee9018762acbbfca3c49ab822
Raymond Zou <raymond.yh.zou@gmail.com>
2026-08-09 16:58:57 -0700

    Gates

--- Assets/Scripts/World/Door.cs
+++ Assets/Scripts/World/Door.cs
@@ -3,10 +3,26 @@ using UnityEngine;
 public class Door : Entity
 {
     [SerializeField] public Channel channel;
+    [SerializeField] Sprite closedSprite;
+    [SerializeField] Sprite openSprite;
     
     public override bool IsSolid()
     {
         return !board.IsChannelActive(channel);
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+
+        if (IsSolid())
+        {
+            spriteRenderer.sprite = closedSprite;
+        }
+        else
+        {
+            spriteRenderer.sprite = openSprite;
+        }
     }
 
 }
